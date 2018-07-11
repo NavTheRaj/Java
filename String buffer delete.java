332163be661769e8b9b021646c80192ac529a913
Nirnav7 <34544499+Nirnav7@users.noreply.github.com>
2018-07-11 07:18:05 +0545

    Create String buffer delete.java

---
+++ String buffer delete.java
@@ -0,0 +1,12 @@
+//delete()
+
+class StringBufferDelete{
+  public static void main(String args[]){
+    
+    StringBuffer sb1=new StringBuffer("Noon");
+    
+    sb1.delete(1,4);  
+    System.out.println(sb1);
+  }
+}
+
