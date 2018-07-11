d976de9023a7b6819e8abfd6aa2434ee4122dd69
Nirnav7 <34544499+Nirnav7@users.noreply.github.com>
2018-07-11 07:16:26 +0545

    Create String Buffer declaration.java

---
+++ String Buffer declaration.java
@@ -0,0 +1,16 @@
+//2018/11/07
+/*
+ Declaration of string buffer
+ StringBuffer variablename=new StringBuffer(" ");
+ 
+ StringBuffer sc=new StringBuffer("Navraj");
+ */
+class StringBufferExamples{
+  public static void main(String args[]){
+    
+    StringBuffer sb1=new StringBuffer("Nirvan");
+    
+    sb1.append(" Khanal");  
+    System.out.println(sb1);
+  }
+}
